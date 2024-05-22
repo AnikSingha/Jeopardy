9c20d8ddeda0f85179cf0bde287e587cbd991b1f
Anik Singha <aniksingha@outlook.com>
2024-05-22 19:07:06 -0400

    create buzzer functionality

--- server/main.go
+++ server/main.go
@@ -3,23 +3,65 @@ package main
 import (
 	"log"
 	"net/http"
-
+	"time"
+	"sync"
 	"github.com/gorilla/websocket"
+	"encoding/json"
 )
 
+type Message struct {
+	User string 		`json:"user"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+type Notification struct {
+	MessageType string    `json:"type"`
+	User        string    `json:"user"`
+}
+
 var upgrader = websocket.Upgrader{}
+var mutex sync.Mutex
+var MessageInfo []Message
+var connections []*websocket.Conn
 
-func main() {
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+	defer conn.Close()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w,r, nil)
+	connections = append(connections, conn)
+
+	for {
+		_, mes, err := conn.ReadMessage()
 		if err != nil {
 			log.Println(err)
 		}
 
-		defer conn.Close()
+		var message Message
+		if err := json.Unmarshal(mes, &message); err != nil {
+			log.Println(err)
+			continue
+		}
 
-	})
+		mutex.Lock()
+		MessageInfo = append(MessageInfo, message)
+		mutex.Unlock()
 
-	http.ListenAndServe("localhost:3000", nil)
+		for _, c := range connections { 
+			err := c.WriteJSON(message)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handleConnections)
+	err := http.ListenAndServe("localhost:3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
